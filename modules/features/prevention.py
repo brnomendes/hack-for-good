72e6265cc90862d909fe91fb7698384ecb638950
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2020-04-05 09:47:18 -0300

    Add images in prevention

--- modules/features/prevention.py
+++ modules/features/prevention.py
@@ -1,3 +1,4 @@
+from flask import session
 from modules.module import Module
 
 
@@ -6,12 +7,24 @@ class Prevention(Module):
 
     def __init__(self, sender, number):
         Module.__init__(self, sender, number)
+        self._last_image_index_key = f"{self.NAME}-image"
 
     def handle(self, content):
         Module.handle(self, content)
         if not content:
             for content in self._content["contents"]:
                 self._enqueue_response(content)
+            
+            index = session.get(self._last_image_index_key, 0)
+            index = index + 1
+
+            images = self._content["images"]
+            self._enqueue_response_media(
+                self._get_static_file(images[index % len(images)], self.NAME)
+            )
+
+            session[self._last_image_index_key] = index
+
 
         self._enqueue_response(self._content["exit"])
         self._send()
